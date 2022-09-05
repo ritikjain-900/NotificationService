f2a9eec12c09b126d67b81ca76440ef94fc7f8ad
ritikjain-900 <ritik.jain@meesho.com>
2022-09-05 16:22:33 +0530

    Deleting everything from master

--- src/main/java/com/meesho/ritik/NotificationService/repository/BlacklistRepository.java
+++
@@ -1,9 +0,0 @@
-package com.meesho.ritik.NotificationService.repository;
-
-import com.meesho.ritik.NotificationService.model.Blacklist;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BlacklistRepository extends JpaRepository<Blacklist, String> {
-}
